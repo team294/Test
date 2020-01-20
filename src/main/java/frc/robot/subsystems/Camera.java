e3c92a16a6ce24e9498a0f1b08e2bc855a86ec25
Don Sawdai <dsawdai@ieee.org>
2020-01-19 17:23:39 -0800

    Added serial port output

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -9,11 +9,14 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoMode;
 
 public class Camera extends SubsystemBase {
   private UsbCamera driveCamera;
+  private SerialPort cameraSerialPort, cameraSerialPort1, cameraSerialPort2;
 
   /**
    * Creates a new Camera.
@@ -30,10 +33,35 @@ public class Camera extends SubsystemBase {
     } catch (Throwable t) {
       t.printStackTrace();
     }
+
+    cameraSerialPort = null;
+
+    SmartDashboard.putString("Camera Port1", "OK");
+    try {
+      cameraSerialPort1 = new SerialPort(115200, SerialPort.Port.kUSB1);
+      cameraSerialPort = cameraSerialPort1;
+    } catch (Throwable t) {
+      t.printStackTrace();
+      SmartDashboard.putString("Camera Port1", t.toString());
+    }
+
+    SmartDashboard.putString("Camera Port2", "OK");
+    try {
+      cameraSerialPort2 = new SerialPort(115200, SerialPort.Port.kUSB2);
+      if (cameraSerialPort == null) cameraSerialPort = cameraSerialPort2;
+    } catch (Throwable t) {
+      t.printStackTrace();
+      SmartDashboard.putString("Camera Port2", t.toString());
+    }
+
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if (cameraSerialPort != null) {
+      String curr = cameraSerialPort.readString();
+      System.out.println("Camera periodic: " +  curr);
+    }
   }
 }
